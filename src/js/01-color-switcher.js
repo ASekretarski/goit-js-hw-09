c43b4f72d15e2f8ac0ba27b18e055b16af17aaff
ASekretarski <artur.sekretarski@gmail.com>
2023-05-28 17:23:54 +0200

    e

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,7 +1,8 @@
 const page = document.querySelector("body");
 const startButton = document.querySelector('button[data-start]')
 const stopButton = document.querySelector('button[data-stop]')
-stopButton.setAttribute("disabled","")
+stopButton.setAttribute("disabled", "")
+let colorChange = null;
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
