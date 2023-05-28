fcc425bbc95c898dcd87f0ce6d7f6a65c22e914d
ASekretarski <119225781+ASekretarski@users.noreply.github.com>
2023-05-28 14:51:09 +0000

    Deploying to gh-pages from @ ASekretarski/goit-js-hw-09@62834235ac0b63b7398b3318a8d1be71c23e0861 🚀

---
+++ 01-color-switcher.191f581f.js
@@ -0,0 +1,2 @@
+!function(){document.querySelector("body");var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");e.setAttribute("disabled",""),t.addEventListener("click",(function(){t.setAttribute("disabled",""),e.removeAttribute("disabled")})),e.addEventListener("click",(function(){t.removeAttribute("disabled"),e.setAttribute("disabled","")}))}();
+//# sourceMappingURL=01-color-switcher.191f581f.js.map
--- 01-color-switcher.90da761f.js
+++
@@ -1,2 +0,0 @@
-!function(){document.querySelector("body");var t=document.querySelector("button[data-start]");document.querySelector("button[data-stop]").setAttribute("disabled",""),t.addEventListener("click",(function(){t.setAttribute("alt","test")}))}();
-//# sourceMappingURL=01-color-switcher.90da761f.js.map
