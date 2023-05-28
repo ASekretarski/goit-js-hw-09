32f517397d3ebd7ac999d2f2b54592a64c945271
ASekretarski <artur.sekretarski@gmail.com>
2023-05-28 16:47:45 +0200

    e

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -2,31 +2,31 @@ const page = document.querySelector("body");
 const startButton = document.querySelector('button[data-start]')
 const stopButton = document.querySelector('button[data-stop]')
 stopButton.setAttribute("disabled","")
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// }
 
-function changeBackgroundColor() {
-    page.style.backgroundColor=getRandomHexColor()
-}
+// function changeBackgroundColor() {
+//     page.style.backgroundColor=getRandomHexColor()
+// }
 
-function backgroundColorSelector() {
-    backgroundColor = setInterval(changeBackgroundColor, 1000);
-}
+// function backgroundColorSelector() {
+//     backgroundColor = setInterval(changeBackgroundColor, 1000);
+// }
 
-function stopBackgroundColorSelector() {
-    clearInterval(backgroundColor);  
-}
+// function stopBackgroundColorSelector() {
+//     clearInterval(backgroundColor);  
+// }
 
-function startButtonDisable() {
-    startButton.setAttribute("disabled", "");
-    stopButton.removeAttribute("disabled")
-}
+// function startButtonDisable() {
+//     startButton.setAttribute("disabled", "");
+//     stopButton.removeAttribute("disabled")
+// }
 
-function stopButtonDisable() {
-    startButton.removeAttribute("disabled");
-    stopButton.setAttribute("disabled","")
-}
+// function stopButtonDisable() {
+//     startButton.removeAttribute("disabled");
+//     stopButton.setAttribute("disabled","")
+// }
 
 // startButton.addEventListener("click", () => {
 //     backgroundColorSelector();
