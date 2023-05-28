f9c314529db734533d6c8dfca592c0084e434da3
ASekretarski <119225781+ASekretarski@users.noreply.github.com>
2023-05-28 15:27:00 +0000

    Deploying to gh-pages from @ ASekretarski/goit-js-hw-09@669208339c3239ae62167d7dc8870ba7f50516f6 🚀

---
+++ 01-color-switcher.21f80318.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("body"),e=document.querySelector("button[data-start]"),r=document.querySelector("button[data-stop]");r.setAttribute("disabled","");let d=null;function o(){t.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}e.addEventListener("click",(()=>{d=setInterval((()=>{o()}),1e3),e.setAttribute("disabled",""),r.removeAttribute("disabled")})),r.addEventListener("click",(()=>{clearInterval(d),r.setAttribute("disabled",""),e.removeAttribute("disabled")}));
+//# sourceMappingURL=01-color-switcher.21f80318.js.map
