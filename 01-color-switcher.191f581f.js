e4b98920e4a87e520debdee30d0d90763b22d3bc
ASekretarski <119225781+ASekretarski@users.noreply.github.com>
2023-05-28 14:53:14 +0000

    Deploying to gh-pages from @ ASekretarski/goit-js-hw-09@8f1e3268c2274b2e79f7ec4dc92c782eb5b45a07 🚀

--- 01-color-switcher.191f581f.js
+++
@@ -1,2 +0,0 @@
-!function(){document.querySelector("body");var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");e.setAttribute("disabled",""),t.addEventListener("click",(function(){t.setAttribute("disabled",""),e.removeAttribute("disabled")})),e.addEventListener("click",(function(){t.removeAttribute("disabled"),e.setAttribute("disabled","")}))}();
-//# sourceMappingURL=01-color-switcher.191f581f.js.map
--- 01-color-switcher.b70b551e.js
+++
@@ -1,2 +0,0 @@
-document.querySelector("body");const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");e.setAttribute("disabled",""),t.addEventListener("click",(()=>{t.setAttribute("disabled",""),e.removeAttribute("disabled")})),e.addEventListener("click",(()=>{t.removeAttribute("disabled"),e.setAttribute("disabled","")}));
-//# sourceMappingURL=01-color-switcher.b70b551e.js.map
