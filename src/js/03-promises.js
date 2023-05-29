8d8dfed906ea338c2218d338b9f6de12e7e4654e
ASekretarski <artur.sekretarski@gmail.com>
2023-05-29 19:10:27 +0200

    task 03 completed

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,30 @@
+import Notiflix from 'notiflix';
+Notiflix.Notify.init({ useIcon: false });
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve(Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`))
+      } else {
+        reject(Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`))
+      }
+    }, delay)
+  })
+}
+
+const form = document.querySelector('.form');
+const firstDelayInput = document.querySelector('input[name="delay"]')
+const delayStepInput = document.querySelector('input[name="step"]')
+const amountInput = document.querySelector('input[name="amount"]')
+
+function formHandler(event) {
+  event.preventDefault();
+  for (let i = 0; i < amountInput.value; i ++){
+    const delay = Number(firstDelayInput.value) + (i * delayStepInput.value);
+    const position = i + 1;
+    createPromise(position, delay)
   }
 }
+
+form.addEventListener("submit", formHandler)
